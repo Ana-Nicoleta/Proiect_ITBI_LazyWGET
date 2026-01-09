a9515d0bc117f8bd8b3a05aefa49465d22d2dc85
Rosca Ana-Nicoleta <ananicoletaigor@gmail.com>
2026-01-09 16:27:54 +0000

    În acest commit modificările sunt destul de majore deoarece înțelesesem greșit cerința, așa că am modificat : ordinea descărcărilor + parsărilor (înainte același fișier îl descărcam și parsam pe niveluri diferite) ; păstrarea "promisiunilor" am modificat-o pentru a fi sub formă de linii de text într-un fișier; am mai creat un fișier pentru contor, ca acesta să nu se reinițializeze cu 1 la fiecare apel, ci să existe o continuitate ; iar extragerea link-urilor o fac cu grep în codul final deoarece varianta cu slice nu lua în considerare și link-urile care nu au "a" înainte de "href", sau care ar putea apărea într-o altă structură (cum ar fi <li><a href="..."</a></li>).

--- lwget.sh
+++ lwget.sh
@@ -1,37 +1,32 @@
 #!/bin/bash
 
-urmeazaVerif="/home/$USER/urmeazaVerif"
-verificate="/home/$USER/verificate"
-fisierHTML=$1
+folder_wget="/home/$USER/folder_wget"
+fisier_evidenta="/home/$USER/fisier_evidenta"
+fisier_evidenta_secund="/home/$USER/fisier_evidenta_secund"
+fisier_cnt="/home/$USER/fisier_cnt"
+link=$1
 
-
-if ! [ -d "$urmeazaVerif" ] && ! [ -d "$verificate" ]; then
-	mkdir "$urmeazaVerif"
-	mkdir "$verificate"
+if ! [ -d "$folder_wget" ] && ! [ -f "$fisier_evidenta" ]; then
+	mkdir "$folder_wget"
+	touch "$fisier_evidenta"
+	touch "$fisier_cnt"
+	echo "1" > $fisier_cnt
 fi
 
-if [ -z "$(ls "$urmeazaVerif")" ]; then
-	if [ -z "$(ls "$verificate")" ]; then #cazul de inceput, prima parsare a html-ului
-		cat $fisierHTML | while read linie;do	
-			link=$(echo "$linie" | awk -F'"' '{print $2}')
-			if [ ! -z "$link" ] && [ "${linie:0:7}" == "<a href" ];then
-				echo $link
-				wget -q -O $urmeazaVerif/$link $link
-				echo "done"
-			fi
-		done 
-	else 
-		echo "Toate resursele au fost extrase, se gasesc la $verificate !"
-	fi
+if [ -z "$(ls "$folder_wget")" ]; then
+		wget -q -O $folder_wget/link1 $link
+		grep -Po '(?<=href=")[^"]*' "$folder_wget/link1" >> $fisier_evidenta
 else
-	echo "$(ls -1 "$urmeazaVerif")" | while read link_neverificat;do
-		cat $urmeazaVerif/$link_neverificat | while read linie;do
-			link=$(echo "$linie" | awk -F'"' '{print $2}')
-				if [ ! -z "$link" ]  && [ "${linie:0:7}" == "<a href" ];then
-					echo $link
-					wget -q -O $urmeazaVerif/$link $link
-					echo "done"
-				fi
+	if ! [ -z "$(cat $fisier_evidenta)" ]; then
+		cat $fisier_evidenta | while read linie;do
+			cnt=$(cat $fisier_cnt)
+			((cnt++))
+			wget -q -O $folder_wget/link$cnt $linie
+			grep -Po '(?<=href=")[^"]*' "$folder_wget/link$cnt" > $fisier_evidenta_secund
+			echo $cnt > $fisier_cnt
 		done
-	done
+		cat $fisier_evidenta_secund > $fisier_evidenta	
+	else
+		echo "Toate resursele au fost extrase, se gasesc la $folder_wget !"
+	fi
 fi
